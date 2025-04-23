29dbae46de86004d68dab187fd2f38d5c9255cea
Phong <phonglna27@gmail.com>
2025-04-23 18:18:25 +0700

    First commit

---
+++ course_app/courses/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.name
